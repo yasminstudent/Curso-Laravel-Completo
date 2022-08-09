7a079c75511a24dc41fffcd8d6e983bb82dde71f
yasminstudent <yasminprog02@gmail.com>
2022-08-09 10:58:22 -0300

    criando primeiro controller + fazendo exercicio

--- 03_PrimeirosPassos/routes/web.php
+++ 03_PrimeirosPassos/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [SiteController::class, 'index']);
+Route::get('/sair', [SiteController::class, 'exit']);
